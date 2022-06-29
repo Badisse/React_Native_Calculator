9d2fed4308b4dc44cc352fa4977e3c065ada0bfd
Badisse <badisse.bouabdallahb@laposte.net>
2022-06-29 23:38:44 +0200

    feat: configure store

---
+++ store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import calculatorReducer from './slices/calculatorSlice';
+
+export const store = configureStore({
+    reducer: {
+        calculator: calculatorReducer,
+    },
+})
